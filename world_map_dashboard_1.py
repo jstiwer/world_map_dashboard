84c7bbb1afa31a31b0e53ed0e88eea5ceb9cb339
jstiwer <jsra65@hotmail.com>
2023-02-11 16:57:41 -0500

    New feature: Population

--- world_map_dashboard_1.py
+++ world_map_dashboard_1.py
@@ -1,5 +1,8 @@
 from countryinfo import CountryInfo
+import pypopulation
 
 input_country = input('Enter a country: ')
 country = CountryInfo(input_country)
 print('Capital is: {}'.format(country.capital()))
+print('Alternative spelling: {}'.format(country.alt_spellings()))
+print('Population is: {}'.format(pypopulation.get_population(country.alt_spellings()[0])))
